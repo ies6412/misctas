05c45c2eda99d8139f5a6445dc98c9d90bddd21b
ies6412 <63573807+ies6412@users.noreply.github.com>
2020-07-07 20:05:29 -0500

    listo hasta las lista de citas y descripcion

--- app/src/main/java/com/example/myappoimts/ui/AppoimentAdapter.kt
+++ app/src/main/java/com/example/myappoimts/ui/AppoimentAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.myappoimts.ui
 
 import android.annotation.SuppressLint
+import android.os.Build
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myappoimts.R
 import com.example.myappoimts.model.Appoment
@@ -15,12 +19,32 @@ class AppoimentAdapter
 
     var appoiment=ArrayList<Appoment>()
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+       @RequiresApi(Build.VERSION_CODES.KITKAT)
        @SuppressLint("SetTextI18n")
        fun bind(appoment:Appoment) = with(itemView){
                TwAppointesId.text= context.getString(R.string.item_appoiment_id,appoment.id)
                TwHoraId.text=context.getString(R.string.item_appoiment_hora,appoment.Horacita)
                TwFechaId.text=context.getString(R.string.item_appoiment_fecha,appoment.fecha)
                TwMedicoId.text=context.getString(R.string.item_appoiment_medico,appoment.doctor.name)
+               TwTDeescripcion.text=appoment.descripcion
+               TwespecialdiadId.text=appoment.especialidad.nombre
+               TwTipoConsulta.text=appoment.TipoCita
+               TwEstado.text=appoment.Estado
+           Twfechareserva.text="Esta cita se registro el día: ${appoment.CreadoEn} con la siguiente descripción:"
+
+                imgbutton.setOnClickListener{
+                        TransitionManager.beginDelayedTransition(parent as ViewGroup,AutoTransition())
+                          if(lineardatos.visibility==View.VISIBLE)
+                          {
+                              lineardatos.visibility=View.GONE
+                             imgbutton.setImageResource(R.drawable.ic_expand_more_black_24dp)
+                          }
+                    else {
+                              lineardatos.visibility = View.VISIBLE
+                              imgbutton.setImageResource(R.drawable.ic_expand_less_black_24dp)
+                          }
+                }
+
 
 
            }
